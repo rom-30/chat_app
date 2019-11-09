ddeac168abcf289b6b4a872053ea60c14c714944
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-11-09 19:19:53 +0000

    Use correct root_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully signed in"
-      redirect_to root
+      redirect_to root_path
     else
       flash.now[:error] = "Please try again, with the correct credentials this time okay!!!"
       render :new
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have successfully signed out"
+
     redirect_to login_path
   end
 end
