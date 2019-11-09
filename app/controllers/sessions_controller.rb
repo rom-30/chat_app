f41e1ec80cf4847b7e906303d59b1db529ce87ca
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-11-09 19:05:49 +0000

    Add create and destroy action to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully signed in"
+      redirect_to root
+    else
+      flash.now[:error] = "Please try again, with the correct credentials this time okay!!!"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully signed out"
+    redirect_to login_path
+  end
 end
