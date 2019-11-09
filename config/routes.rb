553be4d08a927dc8e77ac3800c85a742180d1c2d
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-11-09 20:09:50 +0000

    Add post route for message controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  post 'message', to: 'messages#create'
+
   root 'chat_rooms#index'
 end
