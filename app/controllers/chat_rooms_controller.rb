7fe52ec6689de626cf01aed6607ba4f047ab5860
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-11-11 14:35:53 +0000

    Define scope to only print last 20 messages

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -3,6 +3,11 @@ class ChatRoomsController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = Message.all
+    # 2 options
+    # 1. Active record used in controller or
+    # @messages = Message.order(created_at: :asc).last(20)
+    # 2. scope defined in Message model
+    @messages = Message.last_20
+
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+
+  scope :last_20, -> { order(:created_at).last(20) }
 end
