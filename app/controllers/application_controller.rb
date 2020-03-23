fdd9642eb6c2f089317d96c09f880d077e6479a8
Romeo Saint-Albin <r.stalbin@gmail.com>
2020-03-23 17:26:02 +0000

    Remove unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,27 +11,8 @@ class ApplicationController < ActionController::Base
 
   def require_user
     unless logged_in?
-      flash[:error] = "You must be logged in to perform that action"
+      flash[:error] = 'You must be logged in to perform that action'
       redirect_to login_path
     end
   end
-
-  private
-
-  # def logged_in?
-  #   !!current_user
-  # end
-
-  # def current_user
-  #   @current_user ||=
-  #     session[:current_user_id] &&
-  #     User.find_by(id: session[:current_user_id])
-  # end
-
-  # def require_login
-  #   unless logged_in?
-  #     flash[:error] = "You must be logged in to perform this action"
-  #     redirect_to login_path
-  #   end
-  # end
 end
